35acac12792c9af99afa906c28c57761659943c3
Gerd Lödige <gerd.loedige@posteo.de>
2023-08-24 16:50:11 +0200

    update TicketRepository regarding new find methods by priority, status and creationDate

--- backend/src/main/java/de/iav/backend/repository/TicketRepository.java
+++ backend/src/main/java/de/iav/backend/repository/TicketRepository.java
@@ -4,6 +4,12 @@ import de.iav.backend.model.Ticket;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface TicketRepository extends MongoRepository<Ticket, String> {
+    List<Ticket> findAllByPriorityOrderByPriority (String priority);
+    List<Ticket> findAllByStatusOrderByStatus (String status);
+    List<Ticket> findAllByCreationDateOrderByCreationDate (LocalDateTime creationDate);
 }
