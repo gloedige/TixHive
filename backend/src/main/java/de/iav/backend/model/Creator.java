32b550b620f5cb3332e0ac0a1af80c7501c80601
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-01 07:32:17 +0200

    clean class Creator

--- backend/src/main/java/de/iav/backend/model/Creator.java
+++ backend/src/main/java/de/iav/backend/model/Creator.java
@@ -1,11 +1,9 @@
 package de.iav.backend.model;
 
 import org.springframework.data.mongodb.core.mapping.DBRef;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.util.List;
-//@Document(collation = "ticketCreators")
 public record Creator(
     @MongoId
     String ticketCreatorId,
