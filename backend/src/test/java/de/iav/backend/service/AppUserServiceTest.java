a0101f26efa18471799eae1b8853340ad3901a4b
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-07 09:47:33 +0200

    create AppUserServiceTest with dummy Test of AppUserService

---
+++ backend/src/test/java/de/iav/backend/service/AppUserServiceTest.java
@@ -0,0 +1,40 @@
+package de.iav.backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppUserServiceTest {
+
+    @Test
+    void listAllAppUser() {
+    }
+
+    @Test
+    void getAppUserById() {
+    }
+
+    @Test
+    void getAppUserByEmail() {
+    }
+
+    @Test
+    void listAllTicketByAppUserId() {
+    }
+
+    @Test
+    void addAppUser() {
+    }
+
+    @Test
+    void updateAppUserById() {
+    }
+
+    @Test
+    void deleteAppUserById() {
+    }
+
+    @Test
+    void loadUserByUsername() {
+    }
+}
