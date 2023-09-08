04ba979e32e5b430e3974bb5b71c0c6f0c87725b
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-08 12:32:31 +0200

    create AddTicketController with addTicketButton

---
+++ frontend/src/main/java/de/iav/frontend/controller/AddTicketController.java
@@ -0,0 +1,51 @@
+package de.iav.frontend.controller;
+
+import de.iav.frontend.model.TicketWithoutId;
+import de.iav.frontend.service.TicketService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddTicketController implements Initializable {
+
+    private final TicketService ticketService = TicketService.getInstance();
+    @FXML
+    private Button addTicketButton;
+    @FXML
+    private TextField subjectOfNewTicket;
+    @FXML
+    private ChoiceBox priorityOfNewTicket;
+    @FXML
+    private TextField contentOfNewTicket;
+    private String ticketId;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        priorityOfNewTicket.getItems().addAll("low", "medium", "high");
+    }
+    @FXML
+    public void addTicketButton (ActionEvent event) throws IOException{
+
+            TicketWithoutId newTicket = new TicketWithoutId(
+                    subjectOfNewTicket.getText(),
+                    priorityOfNewTicket.getValue().toString(),
+                    "open",
+                    contentOfNewTicket.getText(),
+                    null);
+            ticketService.addTicket(newTicket);
+
+        //sceneSwitchService.saveNewTicketAndSwitchToTicketList(event);
+
+
+    }
+
+
+
+}
