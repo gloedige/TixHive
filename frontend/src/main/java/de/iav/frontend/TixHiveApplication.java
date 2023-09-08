af78f1c9e628d491a5b3b22a0ca43a46dc6fbc7b
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-08 09:16:51 +0200

    create TixHiveApplication as login-scene.fxml

--- frontend/src/main/java/de/iav/frontend/TixHiveApplication.java
+++ frontend/src/main/java/de/iav/frontend/TixHiveApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class TixHiveApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(TixHiveApplication.class.getResource("/de/iav/frontend/login-scene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 360, 640);
+        stage.setTitle("Login page!");
         stage.setScene(scene);
         stage.show();
     }
