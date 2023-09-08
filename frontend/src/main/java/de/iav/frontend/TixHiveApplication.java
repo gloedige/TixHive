5f695387291083687e33d8fe2d013c44b8a60e05
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-08 11:45:49 +0200

    change title to "new Ticket"

--- frontend/src/main/java/de/iav/frontend/TixHiveApplication.java
+++ frontend/src/main/java/de/iav/frontend/TixHiveApplication.java
@@ -10,14 +10,14 @@ import java.io.IOException;
 public class TixHiveApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TixHiveApplication.class.getResource("/de/iav/frontend/login-scene.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 360, 640);
-        stage.setTitle("Login page!");
+        FXMLLoader fxmlLoader = new FXMLLoader(TixHiveApplication.class.getResource("/de/iav/frontend/fxml/addTicket-scene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("new Ticket");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
