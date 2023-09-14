16d4abcdc4ea30cc89c778000e9c6585863dafbf
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-14 16:37:37 +0200

    rework TicketRequestDTO with @NoArgs*, @AllArgsContructor and @Data

--- backend/src/main/java/de/iav/backend/dto/TicketRequestDTO.java
+++ backend/src/main/java/de/iav/backend/dto/TicketRequestDTO.java
@@ -1,31 +1,16 @@
 package de.iav.backend.dto;
 
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-public class TicketRequestDTO{
-        private String subject;
-        private String priority;
-        private String status;
-        private String text;
-        private String creatorId;
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-    public void setCreatorId(String creatorId) {
-        this.creatorId = creatorId;
-    }
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TicketRequestDTO {
+    private String subject;
+    private String priority;
+    private String status;
+    private String text;
+    private String creatorId;
 }
