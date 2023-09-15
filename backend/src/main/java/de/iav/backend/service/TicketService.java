a84425c7f080fba0795c9ce440218459785c4b0c
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-15 16:43:38 +0200

    update TicketService

--- backend/src/main/java/de/iav/backend/service/TicketService.java
+++ backend/src/main/java/de/iav/backend/service/TicketService.java
@@ -6,9 +6,6 @@ import de.iav.backend.repository.TicketRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 public class TicketService {
@@ -21,38 +18,11 @@ public class TicketService {
         this.idService = idService;
         this.dateTimeService = dateTimeService;
     }
-    public List<Ticket> listAllTicket(){
-        return ticketRepository.findAll();
-    }
-    public Optional<Ticket> getTicketById (String ticketId){
-        return ticketRepository.findById(ticketId);
-    }
-    public List<Ticket> getTicketByPriority (String priority){
-        return ticketRepository.findAllByPriorityOrderByPriority(priority);
-    }
-    public List<Ticket> getTicketByStatus (String status){
-        return ticketRepository.findAllByStatusOrderByStatus(status);
-    }
-    public List<Ticket> getTicketByCreationDate (LocalDateTime creationDate){
-        return ticketRepository.findAllByCreationDateOrderByCreationDate(creationDate);
-    }
     public Ticket addTicket(TicketRequestDTO ticketRequest)
     {
         Ticket newticket = convertToEntity(ticketRequest);
         return ticketRepository.save(newticket);
     }
-    public Ticket updateTicketById(String ticketId, Ticket ticketToUpdate){
-        Optional<Ticket> existingTicket = ticketRepository.findById(ticketId);
-        if(existingTicket.isPresent()){
-            return ticketRepository.save(ticketToUpdate);
-        }
-        throw new NoSuchElementException("Element with "+ ticketId +" not found!");
-    }
-    public void deleteTicketById(String ticketId){
-        ticketRepository.deleteById(ticketId);
-    }
-
-
     public Ticket convertToEntity(TicketRequestDTO requestDTO) {
         LocalDateTime creationDate = dateTimeService.getCurrentDateTime();
         return new Ticket(
