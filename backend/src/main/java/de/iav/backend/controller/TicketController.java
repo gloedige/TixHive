79dd405a84421f7092d6c4ace6fd611ab06cf19c
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-15 16:42:30 +0200

    update TicketController

--- backend/src/main/java/de/iav/backend/controller/TicketController.java
+++ backend/src/main/java/de/iav/backend/controller/TicketController.java
@@ -4,50 +4,18 @@ import de.iav.backend.dto.TicketRequestDTO;
 import de.iav.backend.model.Ticket;
 import de.iav.backend.service.TicketService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/tixhive/ticket")
 public class TicketController {
     private final TicketService ticketService;
-
-    @GetMapping
-    public List<Ticket> listAllTicket(){
-        return ticketService.listAllTicket();
-    }
-    @GetMapping("/{id}")
-    public Optional<Ticket> getTicketById(@PathVariable String id){
-        return ticketService.getTicketById(id);
-    }
-    @GetMapping("/priority/{priority}")
-    public List<Ticket> getTicketByPriority (@PathVariable String priority) {
-        return ticketService.getTicketByPriority(priority);
-    }
-    @GetMapping("/status/{status}")
-    public List<Ticket> getTicketByStatus (@PathVariable String status){
-        return ticketService.getTicketByStatus(status);
-    }
-    @GetMapping("/creationDate/{creationDate}")
-    public List<Ticket> getTicketByCreationDate(@PathVariable LocalDateTime creationDate){
-        return ticketService.getTicketByCreationDate(creationDate);
-    }
     @PostMapping
     public Ticket addTicket(@RequestBody TicketRequestDTO ticketInfo){
         return ticketService.addTicket(ticketInfo);
     }
-    @PutMapping("/{id}")
-    public Ticket updateTicketById(@PathVariable String id, @RequestBody Ticket ticketToUpdate){
-        return ticketService.updateTicketById(id, ticketToUpdate);
-    }
-    @DeleteMapping("/{id}")
-    public void deleteTicketById(@PathVariable String id){
-        ticketService.deleteTicketById(id);
-    }
-
-
 }
