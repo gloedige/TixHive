638136104d0f278038efbcba8d9b5ee0de9b33f8
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-15 12:53:59 +0200

    clean AppUserRepository

--- backend/src/main/java/de/iav/backend/repository/AppUserRepository.java
+++ backend/src/main/java/de/iav/backend/repository/AppUserRepository.java
@@ -1,11 +1,10 @@
 package de.iav.backend.repository;
 
-import de.iav.backend.security.AppUser;
 import de.iav.backend.model.Ticket;
+import de.iav.backend.security.AppUser;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
