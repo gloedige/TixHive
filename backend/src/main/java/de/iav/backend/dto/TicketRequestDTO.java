86be831805a655b0c71885396d7e2cc7c2bc2037
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-15 17:32:37 +0200

    remove @Data from TicketRequestDTO

--- backend/src/main/java/de/iav/backend/dto/TicketRequestDTO.java
+++ backend/src/main/java/de/iav/backend/dto/TicketRequestDTO.java
@@ -1,12 +1,10 @@
 package de.iav.backend.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 public class TicketRequestDTO {
     private String subject;
     private String priority;
