425c1cb2bb5f0f62b41cf4f8645f77849f14f8db
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-15 17:36:43 +0200

    reverse last commit in TicketRequestDTO

--- backend/src/main/java/de/iav/backend/dto/TicketRequestDTO.java
+++ backend/src/main/java/de/iav/backend/dto/TicketRequestDTO.java
@@ -1,10 +1,12 @@
 package de.iav.backend.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class TicketRequestDTO {
     private String subject;
     private String priority;
