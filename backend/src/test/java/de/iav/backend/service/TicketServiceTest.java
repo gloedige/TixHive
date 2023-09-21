c87bdef17ad5641c49ccaaf70185e39fb54bce0c
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-21 14:05:53 +0200

    create test for listAllTickets

--- backend/src/test/java/de/iav/backend/service/TicketServiceTest.java
+++ backend/src/test/java/de/iav/backend/service/TicketServiceTest.java
@@ -8,6 +8,7 @@ import de.iav.backend.repository.TicketRepository;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -50,5 +51,37 @@ class TicketServiceTest {
         verify(ticketRepository).save(expectedTicket);
         verify(dateTimeService).getCurrentDateTime();
         assertEquals(expectedTicket, addedTicket);
+    }
+
+    @Test
+    void listAllTicket_whenTicketlistNotEmpty_thanReturnTicketList() {
+        //GIVEN
+        Ticket ticket1 = new Ticket(
+                "SampleId1",
+                "subject1",
+                TicketPriority.MEDIUM,
+                TicketStatus.OPEN,
+                "text1",
+                "creatorId1",
+                LocalDateTime.of(2023, 9, 14, 16, 11, 11)
+        );
+        Ticket ticket2 = new Ticket(
+                "SampleId2",
+                "subject2",
+                TicketPriority.MEDIUM,
+                TicketStatus.OPEN,
+                "text2",
+                "creatorId2",
+                LocalDateTime.of(2023, 9, 14, 16, 11, 11)
+        );
+
+        //WHEN
+        when(ticketRepository.findAll()).thenReturn(List.of(ticket1, ticket2));
+        List<Ticket> expectedTicketList = List.of(ticket1, ticket2);
+        var addedTicketList = ticketService.listAllTicket();
+
+        //THEN
+        verify(ticketRepository).findAll();
+        assertEquals(expectedTicketList, addedTicketList);
     }
 }
