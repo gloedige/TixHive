a1be7b6e3570f9925b8d60c085e6c57f7bdeb7a5
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-21 10:46:49 +0200

    update TicketRequestDTO as record

--- backend/src/main/java/de/iav/backend/dto/TicketRequestDTO.java
+++ backend/src/main/java/de/iav/backend/dto/TicketRequestDTO.java
@@ -2,17 +2,11 @@ package de.iav.backend.dto;
 
 import de.iav.backend.model.TicketPriority;
 import de.iav.backend.model.TicketStatus;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-public class TicketRequestDTO {
-    private String subject;
-    private TicketPriority priority;
-    private TicketStatus status;
-    private String text;
-    private String creatorId;
+public record TicketRequestDTO(
+        String subject,
+        TicketPriority priority,
+        TicketStatus status,
+        String text,
+        String creatorId) {
 }
