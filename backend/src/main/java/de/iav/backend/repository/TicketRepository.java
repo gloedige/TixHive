1132709ea88c973d410ca5f68f19f029d5a229b8
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-21 12:22:08 +0200

    clear TicketRepository backend

--- backend/src/main/java/de/iav/backend/repository/TicketRepository.java
+++ backend/src/main/java/de/iav/backend/repository/TicketRepository.java
@@ -4,9 +4,6 @@ import de.iav.backend.model.Ticket;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Repository
 public interface TicketRepository extends MongoRepository<Ticket, String> {
 }
