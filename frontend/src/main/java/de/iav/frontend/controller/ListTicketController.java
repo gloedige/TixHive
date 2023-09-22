4da7877ea1212581cfa8e60b466068b552879648
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-22 12:14:30 +0200

    add feature listTickets to ListTicketController

--- frontend/src/main/java/de/iav/frontend/controller/ListTicketController.java
+++ frontend/src/main/java/de/iav/frontend/controller/ListTicketController.java
@@ -4,16 +4,16 @@ import de.iav.frontend.model.Ticket;
 import de.iav.frontend.model.TicketPriority;
 import de.iav.frontend.model.TicketStatus;
 import de.iav.frontend.service.TicketService;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -27,25 +27,23 @@ public class ListTicketController {
     private final TicketService ticketService = TicketService.getInstance();
 
     @FXML
-    private Button addTicketButton;
-    @FXML
     private TableView<Ticket> table;
     @FXML
-    private TableColumn<Ticket, String> subjectColumn;
+    private TableColumn<Ticket, String> subjectColumn = new TableColumn<>("Subject");
     @FXML
-    private TableColumn<Ticket, TicketPriority> priorityColumn;
+    private TableColumn<Ticket, TicketPriority> priorityColumn = new TableColumn<>("Priority");
     @FXML
-    private TableColumn<Ticket, TicketStatus> statusColumn;
+    private TableColumn<Ticket, TicketStatus> statusColumn = new TableColumn<>("Status");
     @FXML
-    private TableColumn<Ticket, LocalDateTime> creationDateColumn;
+    private TableColumn<Ticket, LocalDateTime> creationDateColumn = new TableColumn<>("Creation date");
 
     public void initialize() {
         List<Ticket> allTicket = ticketService.listAllTickets();
         table.getItems().clear();
-        subjectColumn.setCellValueFactory(new PropertyValueFactory<>("subject"));
-        priorityColumn.setCellValueFactory(new PropertyValueFactory<>("priority"));
-        statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
-        creationDateColumn.setCellValueFactory(new PropertyValueFactory<>("creationDate"));
+        subjectColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().subject()));
+        priorityColumn.setCellValueFactory(p -> new SimpleObjectProperty<>(p.getValue().priority()));
+        statusColumn.setCellValueFactory(p -> new SimpleObjectProperty<>(p.getValue().status()));
+        creationDateColumn.setCellValueFactory(p -> new SimpleObjectProperty<>(p.getValue().creationDate()));
 
         table.getItems().addAll(allTicket);
     }
