d609c8ca199fecb0856f51b7f6803e2fe77b95b4
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-29 18:24:23 +0200

    create LoginController (not working)

---
+++ frontend/src/main/java/de/iav/frontend/controller/LoginController.java
@@ -0,0 +1,80 @@
+package de.iav.frontend.controller;
+
+import de.iav.frontend.exception.CustomIOException;
+import de.iav.frontend.security.AppUserRole;
+import de.iav.frontend.security.AuthService;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private TextField usernameInput;
+    @FXML
+    private PasswordField passwordInput;
+    @FXML
+    private Label errorLabel;
+    @FXML
+    private ChoiceBox<String> roleChoiceBox;
+
+    private final AuthService authService = AuthService.getInstance();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        roleChoiceBox.getItems().addAll(AppUserRole.USER.toString(), AppUserRole.ADMIN.toString(), AppUserRole.DEVELOPER.toString());
+    }
+
+    @FXML
+    protected void onLoginClick() {
+        login();
+    }
+
+    @FXML
+    private void login() {
+        if (authService.login(usernameInput.getText(), passwordInput.getText())) {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/de/iav/frontend/fxml/listAllTickets-scene.fxml"));
+
+            try {
+                root = fxmlLoader.load();
+            } catch (Exception e) {
+                throw new CustomIOException(e.toString());
+
+            }
+
+            scene = new Scene(root);
+            stage = (Stage) usernameInput.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Ticket List");
+        } else {
+            errorLabel.setText(authService.errorMessage());
+        }
+    }
+
+    @FXML
+    protected void onRegisterClick() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/de/iav/frontend/fxml/register-scene.fxml"));
+
+        try {
+            root = fxmlLoader.load();
+        } catch (Exception e) {
+            throw new CustomIOException(e.toString());
+        }
+
+        scene = new Scene(root);
+        stage = (Stage) usernameInput.getScene().getWindow();
+        stage.setScene(scene);
+    }
+}
