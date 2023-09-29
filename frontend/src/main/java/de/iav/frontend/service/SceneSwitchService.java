b4cbd760885097a580e14b0eb1412e8c62789ad4
Gerd Lödige <gerd.loedige@posteo.de>
2023-09-29 11:24:56 +0200

    update SceneSwitchService add title to switchToTicketListScene

--- frontend/src/main/java/de/iav/frontend/service/SceneSwitchService.java
+++ frontend/src/main/java/de/iav/frontend/service/SceneSwitchService.java
@@ -33,6 +33,7 @@ public class SceneSwitchService {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setTitle("Ticket List");
         stage.show();
     }
 }
