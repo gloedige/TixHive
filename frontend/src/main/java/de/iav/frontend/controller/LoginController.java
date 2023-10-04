1104cd3283a1731bb6fb603cad3622be006fb054
Gerd Lödige <gerd.loedige@posteo.de>
2023-10-04 15:41:01 +0200

    update AuthService and LoginController - delete AppUserRole from login process

--- frontend/src/main/java/de/iav/frontend/controller/LoginController.java
+++ frontend/src/main/java/de/iav/frontend/controller/LoginController.java
@@ -1,23 +1,17 @@
 package de.iav.frontend.controller;
 
 import de.iav.frontend.exception.CustomIOException;
-import de.iav.frontend.security.AppUserRole;
 import de.iav.frontend.security.AuthService;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class LoginController implements Initializable {
+public class LoginController {
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -27,15 +21,8 @@ public class LoginController implements Initializable {
     private PasswordField passwordInput;
     @FXML
     private Label errorLabel;
-    @FXML
-    private ChoiceBox<String> roleChoiceBox;
 
     private final AuthService authService = AuthService.getInstance();
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        roleChoiceBox.getItems().addAll(AppUserRole.USER.toString(), AppUserRole.ADMIN.toString(), AppUserRole.DEVELOPER.toString());
-    }
 
     @FXML
     protected void onLoginClick() {
@@ -44,15 +31,7 @@ public class LoginController implements Initializable {
 
     @FXML
     private void login() {
-        String selectedValue = roleChoiceBox.getValue();
-        AppUserRole selectedRole;
-        if (selectedValue.equals(AppUserRole.USER.toString())) {
-            selectedRole = AppUserRole.USER;
-        } else if (selectedValue.equals(AppUserRole.ADMIN.toString())) {
-            selectedRole = AppUserRole.ADMIN;
-        } else selectedRole = AppUserRole.DEVELOPER;
-
-        if (authService.login(usernameInput.getText(), passwordInput.getText(), selectedRole)) {
+        if (authService.login(usernameInput.getText(), passwordInput.getText())) {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/de/iav/frontend/fxml/listAllTickets-scene.fxml"));
 
             try {
