e6c8048430ef075fb66a3bc93f6368266794f508
Gerd Lödige <gerd.loedige@posteo.de>
2023-10-11 17:13:03 +0200

    create addTicketToAppUser in backend UserController

--- backend/src/main/java/de/iav/backend/controller/UserController.java
+++ backend/src/main/java/de/iav/backend/controller/UserController.java
@@ -1,21 +1,51 @@
 package de.iav.backend.controller;
 
+import de.iav.backend.model.Ticket;
+import de.iav.backend.repository.TicketRepository;
 import de.iav.backend.security.AppUser;
+import de.iav.backend.security.AppUserRepository;
 import de.iav.backend.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
+import java.util.ArrayList;
+import java.util.List;
+
+
 @RestController
 @RequestMapping("/api/tixhive/users")
 public class UserController {
     private final UserService userService;
+    private final AppUserRepository appUserRepository;
+    private final TicketRepository ticketRepository;
+
+    public UserController(UserService userService, AppUserRepository appUserRepository, TicketRepository ticketRepository) {
+        this.userService = userService;
+        this.appUserRepository = appUserRepository;
+        this.ticketRepository = ticketRepository;
+    }
 
     @GetMapping("/{email}")
     public AppUser findUserById(@PathVariable String email) {
         return userService.findUserByEmail(email);
+    }
+
+    @PutMapping("/{email}/tickets/{ticketId}")
+    public AppUser addTicketToAppUser(@PathVariable String email, @PathVariable String ticketId) {
+        AppUser appUser = findUserById(email);
+        Ticket ticket = ticketRepository.findById(ticketId).orElseThrow();
+        List<Ticket> newTicketList = appUser.tickets();
+        if (newTicketList == null) {
+            newTicketList = new ArrayList<>();
+        }
+        newTicketList.add(ticket);
+
+        return appUserRepository.save(new AppUser(
+                appUser.id(),
+                appUser.username(),
+                appUser.email(),
+                appUser.password(),
+                appUser.role(),
+                newTicketList
+        ));
     }
 }
