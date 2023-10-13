08f52743490eb963491d0a64045cc1ad4e2dd313
Gerd Lödige <gerd.loedige@posteo.de>
2023-10-13 17:23:24 +0200

    update UserService with findUserByTicketId in case of admin

--- backend/src/main/java/de/iav/backend/service/UserService.java
+++ backend/src/main/java/de/iav/backend/service/UserService.java
@@ -1,5 +1,6 @@
 package de.iav.backend.service;
 
+import de.iav.backend.Exceptions.CustomUserNotFoundException;
 import de.iav.backend.security.AppUser;
 import de.iav.backend.security.AppUserRepository;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,18 @@ public class UserService {
 
     public AppUser findUserByEmail(String email) {
         return appUserRepository.findByEmail(email).orElseThrow();
+    }
+
+    public AppUser findUserByTicketId(String ticketId) {
+        Iterable<AppUser> allUsers = appUserRepository.findAll();
+        for (AppUser user : allUsers) {
+            if (user.tickets() == null) {
+                continue;
+            }
+            if (user.tickets().stream().anyMatch(ticket -> ticket.id().equals(ticketId))) {
+                return user;
+            }
+        }
+        throw new CustomUserNotFoundException("No user found with ticketId: " + ticketId);
     }
 }
