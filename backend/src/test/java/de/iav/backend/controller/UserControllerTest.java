cdab92a787db647e439f448f938ec5e844545abb
Gerd Lödige <gerd.loedige@posteo.de>
2023-10-15 10:51:02 +0200

    create test in UserControllerTest for testFindTicketsByUser_whenUserExist_listAllUserTickets

--- backend/src/test/java/de/iav/backend/controller/UserControllerTest.java
+++ backend/src/test/java/de/iav/backend/controller/UserControllerTest.java
@@ -2,6 +2,10 @@ package de.iav.backend.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.iav.backend.dto.TicketRequestDTO;
+import de.iav.backend.model.Ticket;
+import de.iav.backend.model.TicketPriority;
+import de.iav.backend.model.TicketStatus;
 import de.iav.backend.security.AppUser;
 import de.iav.backend.security.AppUserRequest;
 import de.iav.backend.security.AppUserRole;
@@ -15,6 +19,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.io.IOException;
@@ -35,6 +40,7 @@ class UserControllerTest {
 
     private final String BASE_USER_URL = "/api/tixhive/users";
     private final String BASE_AUTH_URL = "/api/auth/register";
+    private final String BASE_TICKET_URL = "/api/tixhive/tickets";
 
     public static class UserControllerJsonParser {
         private static final ObjectMapper objectMapper = new ObjectMapper();
@@ -74,7 +80,53 @@ class UserControllerTest {
     }
 
     @Test
-    void findTicketsByUser() {
+    void testFindTicketsByUser_whenUserExist_listAllUserTickets() throws Exception {
+        String UserEmail = "UserEmail";
+        AppUserRequest userToFind = new AppUserRequest(
+                "Username",
+                UserEmail,
+                "UserPassword",
+                AppUserRole.USER);
+        String userRequestJson = objectMapper.writeValueAsString(userToFind);
+        mockMvc.perform(post(BASE_AUTH_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userRequestJson))
+                .andExpect(status().is(201))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+
+        TicketRequestDTO ticketRequestDTO = new TicketRequestDTO(
+                "Test Subject",
+                TicketPriority.HIGH,
+                TicketStatus.OPEN,
+                "Test Text",
+                "Test CreatorId");
+        String ticketRequestJson = objectMapper.writeValueAsString(ticketRequestDTO);
+        MvcResult result = mockMvc.perform(post(BASE_TICKET_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(ticketRequestJson))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        String responseJson = result.getResponse().getContentAsString();
+        Ticket addedTicket = objectMapper.readValue(responseJson, Ticket.class);
+        String ticketId = addedTicket.id();
+
+
+        mockMvc.perform(MockMvcRequestBuilders.put(BASE_USER_URL + "/" + UserEmail + "/" + ticketId))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+
+        MvcResult mvcResult = mockMvc.perform(get(BASE_USER_URL + "/" + UserEmail + "/tickets"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        String responseJson2 = mvcResult.getResponse().getContentAsString();
+        Ticket[] foundTickets = objectMapper.readValue(responseJson2, Ticket[].class);
+        Assertions.assertNotNull(foundTickets);
     }
 
     @Test
