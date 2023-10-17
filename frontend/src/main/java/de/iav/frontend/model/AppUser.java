3236ff438fa9bf9d254e7df92033e0704fbd70c6
Gerd Lödige <gerd.loedige@posteo.de>
2023-10-17 10:40:47 +0200

    update frontend AppUser

--- frontend/src/main/java/de/iav/frontend/model/AppUser.java
+++ frontend/src/main/java/de/iav/frontend/model/AppUser.java
@@ -1,21 +1,12 @@
 package de.iav.frontend.model;
-
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.DBRef;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.util.List;
 
-@Document(collection = "users")
 public record AppUser(
         String id,
-        @Indexed(unique = true)
         String username,
-        @Indexed(unique = true)
         String email,
         String password,
         AppUserRole role,
-        @DBRef
         List<Ticket> tickets
 ) {
 }
