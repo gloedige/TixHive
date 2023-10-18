eae630f360708f55ef38ecc2107ddacbe02f1e7e
Gerd Lödige <gerd.loedige@posteo.de>
2023-10-18 12:57:32 +0200

    update pom due to junit jupiter version

--- backend/src/test/java/de/iav/backend/service/UserServiceTest.java
+++ backend/src/test/java/de/iav/backend/service/UserServiceTest.java
@@ -10,8 +10,8 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
